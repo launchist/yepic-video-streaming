b3de52cc9623d2135681e24601160e0b66768ee8
Roman Hamov <romanhamov25@gmail.com>
2022-12-20 16:42:19 +0200

    add store

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ console.log('123')
 const getVideoEndpoint = "https://yepic-email-validation-zckwps36uq-nw.a.run.app/videos/?"
 console.log('now we get it here')
 console.log(321)
+
+const store = {
+    id: new URLSearchParams(location.search).get('id')
+}
+
+document.addEventListener("alpine:init", () => {
+    Alpine.store("data", store);
+  });
+
+console.log('id', store.id)
