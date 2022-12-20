e8445531f5a0fea2a5920fda00e68b71c5fa106a
Roman Hamov <romanhamov25@gmail.com>
2022-12-20 16:52:31 +0200

    add request

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-console.log('123')
-const getVideoEndpoint = "https://yepic-email-validation-zckwps36uq-nw.a.run.app/videos/?"
-console.log('now we get it here')
-console.log(321)
+const getVideoEndpoint =
+  "https://yepic-email-validation-zckwps36uq-nw.a.run.app/videos/";
+const idParam = "?id=";
+console.log("now we get it here");
+
+const requestOptions = {
+  method: "GET",
+  redirect: "follow",
+};
+
+const getVideo = async (id) => {
+    isLoading = true;
+    await fetch(
+      `${getVideoEndpoint}${idParam}${id}`,
+      requestOptions
+    )
+      // .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  };
+
+let isLoading = false;
 
 const store = {
-    id: new URLSearchParams(location.search).get('id')
-}
+  id: new URLSearchParams(location.search).get("id"),
+//   video: await getVideo(new URLSearchParams(location.search).get("id"))
+};
+console.log("id", store.id);
 
 document.addEventListener("alpine:init", () => {
-    Alpine.store("data", store);
-  });
+  Alpine.store("data", store);
+  console.log("init", store);
+  getVideo(store.id)
+});
 
-console.log('id', store.id)
+
