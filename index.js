684d26f683a66db4a63b8dd388c1153f60140844
Roman Hamov <romanhamov25@gmail.com>
2022-12-20 22:08:23 +0200

    add response to store

--- index.js
+++ index.js
@@ -1,36 +1,57 @@
 const getVideoEndpoint =
   "https://yepic-email-validation-zckwps36uq-nw.a.run.app/videos/";
-const idParam = "?id=";
+// const getVideoEndpoint = "https://0a54-207-188-136-240.eu.ngrok.io/videos/";
+const getVideoDetailsSlug = "get-video-details";
+const getVideoSlug = "video";
 console.log("now we get it here");
+
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
 
 const requestOptions = {
   method: "GET",
   redirect: "follow",
+  headers: myHeaders,
 };
 
-const getVideo = async (id) => {
-    isLoading = true;
-    await fetch(
-      `${getVideoEndpoint}${idParam}${id}`,
-      requestOptions
-    )
-      // .then((response) => response.text())
-      .then((result) => console.log(result))
-      .catch((error) => console.log("error", error));
-  };
+const idFromQueryParam = new URLSearchParams(location.search).get("id");
+
+const store = {
+  getVideoEndpoint,
+  id: idFromQueryParam,
+  getStreamUrl: `${getVideoEndpoint}${getVideoSlug}?id=${idFromQueryParam}`,
+  video: null,
+  membership: null,
+};
 
 let isLoading = false;
 
-const store = {
-  id: new URLSearchParams(location.search).get("id"),
-//   video: await getVideo(new URLSearchParams(location.search).get("id"))
+const getMembership = async () => {
+  if (!store.id) return;
+  isLoading = true;
+  let resp = await fetch(
+    `${getVideoEndpoint}${getVideoDetailsSlug}?id=${store.id}`,
+    requestOptions
+  );
+  let res = await resp.json();
+  isLoading = false;
+  return res?.details;
 };
-console.log("id", store.id);
 
-document.addEventListener("alpine:init", () => {
+document.addEventListener("alpine:init", async () => {
   Alpine.store("data", store);
-  console.log("init", store);
-  getVideo(store.id)
+  const video = document.getElementById("my-video");
+  var player = videojs(video);
+
+  player.log('window.player created', player);
+
+  player.loadMedia({
+    src: [{
+      src: store.getStreamUrl,
+      type: 'video/mp4'
+    }]
+  })
+  const membership = await getMembership();
+  store.membership = membership;
+  console.log("init");
 });
-
-
